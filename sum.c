0cb811afc77e4becaae0f0e5f63b9192f9cf8318
Anuj Goyal <anuj.goyal@gmail.com>
2017-10-14 22:33:59 -0700

    align to 16 byte boundaries, create function for adding SSE objects simply

--- sum.c
+++ sum.c
@@ -16,44 +16,59 @@ void p128_as_int(__m128i in) {
     printf("int: %i %i %i %i\n", v[0], v[1], v[2], v[3]);
 }
 
+__m128i add_vector(__m128i *a, __m128i *b) {
+    __m128i r = _mm_add_epi32(*a,*b);
+    return r;
+}
 
 int main() {
 
     // basic addition of two SSE vectors
-    __m128i src = _mm_set_epi32(1,2,3,4);
-    __m128i ops = _mm_set_epi32(5,6,7,8);
+    __m128i src __attribute__((aligned(16))) = _mm_set_epi32(1,2,3,4);
+    __m128i ops __attribute__((aligned(16))) = _mm_set_epi32(5,6,7,8);
     // add using SSE intrinsic
-    __m128i res = _mm_add_epi32(src, ops);
+    __m128i res __attribute__((aligned(16))) = _mm_add_epi32(src, ops);
     // print out values
-    p128_as_char(res); p128_as_short(res); p128_as_int(res);
+    //p128_as_char(res);
+    //p128_as_short(res); 
+    p128_as_int(res);
 
     // try the reverse when using set
-    __m128i y = _mm_set_epi32(4,3,2,1);
-    __m128i z = _mm_set_epi32(8,7,6,5);
-    res = _mm_add_epi32(y,z); printf("\n"); p128_as_int(res);
-
+    //__m128i y = _mm_set_epi32(4,3,2,1), z = _mm_set_epi32(8,7,6,5);
+    //res = add_vector(&y,&z); printf("\n"); p128_as_int(res);
 
     // Sum two SSE __m128i objects
     int a[4] __attribute__((aligned(16))) = {0,1,2,3}; // init to zero, https://stackoverflow.com/questions/201101
     int b[4] __attribute__((aligned(16))) = {1,1,1,1};
     __m128i *pa = (__m128i*)a;
     __m128i *pb = (__m128i*)b;
-    __m128i result = _mm_add_epi32(*pa, *pb);
-    p128_as_int(result);
+    res = _mm_add_epi32(*pa, *pb);
+    p128_as_int(res);
+
+    // try the reverse when using set, also call add_vector
+    printf("\n");
+    __m128i y = _mm_set_epi32(4,3,2,1);
+    __m128i z = _mm_set_epi32(8,7,6,5);
+    res = add_vector(&y,&z);
+    p128_as_int(res);
 
     // sum up values for an array
-    // option 1: large a, one value for b
-    int c[4] __attribute__((aligned(16)));
-    int d[32768] __attribute__((aligned(16)));
-    int e[32768] __attribute__((aligned(16)));
+    // option 1: atom + vector
+    int c[4]     __attribute__((aligned(16))) = {5,5,5,5};
+    int d[32768] __attribute__((aligned(16))) = {1};
+    int e[32768] __attribute__((aligned(16))); // going to be overridden
     __m128i *pc = (__m128i*)c;
     for (int i=0; i<32768; i+=4) {
         __m128i *pd = (__m128i*)(&d[i]);
         __m128i *pe = (__m128i*)(&e[i]);
         *pe = _mm_add_epi32( *pc, *pd);
     }
+    // print first elements of e
+    pc = (__m128i*)&e[0];
+    printf("atom+vector\n");
+    p128_as_int(*pc);
 
-    // option 2: large a, large b
+    // option 2: vector + vector, both must be equal length
 
     //printf("sum: %d\n", result);
     return EXIT_SUCCESS;
