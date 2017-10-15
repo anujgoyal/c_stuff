cf3c9f4ba03ad2a75efcad512b1c6619d5f478fc
Anuj Goyal <anuj.goyal@gmail.com>
2017-10-14 21:05:53 -0700

    char, short, int

--- sum.c
+++ sum.c
@@ -4,12 +4,12 @@
 
 void p128_as_char(__m128i in) {
     uint8_t *v = (uint8_t*) &in;
-    printf("char: %u %u %u %u %u %u %u %u \n%u %u %u %u %u %u %u %u\n",
+    printf("char: %u %u %u %u, %u %u %u %u, %u %u %u %u, %u %u %u %u\n",
     v[0], v[1], v[2], v[3], v[4], v[5], v[6], v[7], v[8], v[9], v[10], v[11], v[12], v[13], v[14], v[15]);
 }
 void p128_as_short(__m128i in) {
     uint16_t *v = (uint16_t*) &in;
-    printf("short: %i %i %i %i %i %i %i %i \n", v[0], v[1], v[2], v[3], v[4], v[5], v[6], v[7]);
+    printf("short: %i %i %i %i, %i %i %i %i\n", v[0], v[1], v[2], v[3], v[4], v[5], v[6], v[7]);
 }
 void p128_as_int(__m128i in) {
     uint32_t *v = (uint32_t*) &in;
@@ -29,8 +29,9 @@ int main() {
 
     __m128i src = _mm_set_epi32(1,2,3,4);
     __m128i ops = _mm_set_epi32(5,6,7,8);
-    // add via SSE instructions
+    // add using SSE intrinsic
     __m128i res = _mm_add_epi32(src, ops);
+    // print out values
     p128_as_char(res);
     p128_as_short(res);
     p128_as_int(res);
