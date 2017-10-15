63139e08d1f6dd30ce8c10f26ee445258b9a7c8c
Anuj Goyal <anuj.goyal@gmail.com>
2017-10-14 21:51:19 -0700

    fun with SSE, try to add two vectors and store the result

--- sum.c
+++ sum.c
@@ -18,23 +18,42 @@ void p128_as_int(__m128i in) {
 
 
 int main() {
-    // init to zero, https://stackoverflow.com/questions/201101
-    //int ar[4] = {0};
-    //int br[4] = {0};
-    //__mm128i *pa = (__m128i*)ar;
-    //__mm128i *pb = (__m128i*)br;
-    //result = _mm_add_epi32(*a, *b);
-    //__m128i a = _mm_set_epi32(1,2,3,4);
-    //__m128i b = _mm_set_epi32(1,2,3,4);
 
+    // basic addition of two SSE vectors
     __m128i src = _mm_set_epi32(1,2,3,4);
     __m128i ops = _mm_set_epi32(5,6,7,8);
     // add using SSE intrinsic
     __m128i res = _mm_add_epi32(src, ops);
     // print out values
-    p128_as_char(res);
-    p128_as_short(res);
-    p128_as_int(res);
+    p128_as_char(res); p128_as_short(res); p128_as_int(res);
+
+    // try the reverse when using set
+    __m128i y = _mm_set_epi32(4,3,2,1);
+    __m128i z = _mm_set_epi32(8,7,6,5);
+    res = _mm_add_epi32(y,z); printf("\n"); p128_as_int(res);
+
+
+    // Sum two SSE __m128i objects
+    int a[4] __attribute__((aligned(16))) = {0,1,2,3}; // init to zero, https://stackoverflow.com/questions/201101
+    int b[4] __attribute__((aligned(16))) = {1,1,1,1};
+    __m128i *pa = (__m128i*)a;
+    __m128i *pb = (__m128i*)b;
+    __m128i result = _mm_add_epi32(*pa, *pb);
+    p128_as_int(result);
+
+    // sum up values for an array
+    // option 1: large a, one value for b
+    int c[4] __attribute__((aligned(16)));
+    int d[32768] __attribute__((aligned(16)));
+    int e[32768] __attribute__((aligned(16)));
+    __m128i *pc = (__m128i*)c;
+    for (int i=0; i<32768; i+=4) {
+        __m128i *pd = (__m128i*)(&d[i]);
+        __m128i *pe = (__m128i*)(&e[i]);
+        *pe = _mm_add_epi32( *pc, *pd);
+    }
+
+    // option 2: large a, large b
 
     //printf("sum: %d\n", result);
     return EXIT_SUCCESS;
