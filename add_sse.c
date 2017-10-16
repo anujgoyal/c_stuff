172b90cff3d78365272107af01b9c7604578f699
Anuj Goyal <anuj.goyal@gmail.com>
2017-10-15 18:50:14 -0700

    improvements

--- add_sse.c
+++ add_sse.c
@@ -1,6 +1,9 @@
+// this code will curreonly only run on x86 systems
+// https://software.intel.com/sites/default/files/m/a/9/b/7/b/1000-SSE.pdf
+// SSE means 16 bytes, 4 32-bit integers, 2 64-bit integers, 8 16-bit shorts 
 #include <stdlib.h>
 #include <stdio.h>
-//#include <math.h>
+#include <x86intrin.h>
 
 #define N 32768
 
@@ -8,11 +11,23 @@ int main()
 {
     // init to zero, https://stackoverflow.com/questions/201101
     int a[N] = {};
+    __m128i d = 0;
+    __m128i a = 0;
+    __m128i b = 1;
     int sum=0;
 
+    // add by looping through each array element
     for (int i=0; i<N; ++i) {
        sum += a[i]; 
     }
+
+    // add via SSE instructions
+    for (int i=0; i<N; i+=4)
+//       sum += _mm_add_ps((__m128i)a[i], d);
+    }
+
+_mm_hadd_epi32(__m128i a, __m128i b)
+
 
     printf("sum: %d\n", sum);
     return EXIT_SUCCESS;
